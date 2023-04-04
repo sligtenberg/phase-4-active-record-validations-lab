0c3c4c961031e668ae0a72e91948e32a9faddd5c
stephen ligtenberg <bountyhunter24@gmail.com>
2023-04-03 22:04:45 -0600

    commit tree b41dabfc5c5e40ebd4adbd8d9caea7b98409c408

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+
+    validate :clickbait?
+
+    def clickbait?
+        if title
+            unless title.include?("Won't Believe") || title.include?("Secret") || title.include?("Top [number]") || title.include?("Guess")
+                errors.add(:title, "Title must be clickbait-y")
+            end
+        end
+    end
 end
